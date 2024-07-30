c174cb30125160020edcdc9f381ffe12b417511d
Jitendra Kumar Pandit <121290619+jkjitendra100@users.noreply.github.com>
2024-07-30 07:56:13 +0530

    Add files via upload

--- routes/withdraw.js
+++ routes/withdraw.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middlewares/auth.js";
 
-import { addWithdrawRequest } from "../controllers/withdraw.js";
+import {
+  addWithdrawRequest,
+  getAllRequests,
+  markAsPaid,
+} from "../controllers/withdraw.js";
 
 const router = express.Router();
 
 router.post("/new", isAuthenticated, addWithdrawRequest);
+router.put("/update/:requestId", isAuthenticated, markAsPaid);
+router.get("/all/:pageNo/:pageSize", isAuthenticated, getAllRequests);
 
 export default router;
